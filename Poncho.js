94933a795958a658caecf52b43d4d56aca958b1f
Sophivorus <schenonef@gmail.com>
2018-07-23 00:12:50 -0300

    Upgrade to 1.2
    Change-Id: I65bbcbb9ace2feb528c633915a0a1bdf5da7cbae

--- Poncho.js
+++ Poncho.js
@@ -1,66 +1,7 @@
-var poncho = {
+var Poncho = {
 
-	init: function () {
-		poncho.bind();
+	// Empty for now
 
-/*
-		// Push the footer to the bottom
-	    var heightDiff = $( window ).height() - $( 'body' ).height();
-	    if ( heightDiff > 0 ) {
-	        $( '#footer' ).css( 'margin-top', heightDiff );
-	    }
-*/
-	},
-
-	bind: function () {
-		$( '#menu > li' ).mouseover( poncho.showSubmenu ).mouseout( poncho.hideSubmenu );
-		$( '#search-form button' ).click( poncho.showSearchInput );
-		$( '#search-form input' ).focusout( poncho.hideSearchInput );
-	},
-
-	showSubmenu: function () {
-		var menu = $( this ),
-			submenu = $( '.submenu', menu );
-		if ( !submenu.is( ':visible' ) ) {
-			submenu.show();
-		}
-	},
-
-	hideSubmenu: function () {
-		var menu = $( this );
-		menu.children( '.submenu' ).hide();
-	},
-
-	toggleSubmenu: function () {
-		var menu = $( this ),
-			submenu = $( '.submenu', menu );
-		if ( submenu.is( ':visible' ) ) {
-			submenu.hide();
-		} else {
-			submenu.show();
-		}
-	},
-
-	showSearchInput: function () {
-		var searchButton = $( this ),
-			searchForm = searchButton.parent(),
-			searchInput = $( 'input', searchForm );
-
-		if ( !searchInput.is( ':visible' ) ) {
-			searchInput.show().focus();
-			searchButton.addClass( 'active' );
-			return false;
-		}
-	},
-
-	hideSearchInput: function () {
-		var searchInput = $( this ),
-			searchForm = searchInput.parent(),
-			searchButton = $( 'button', searchForm );
-
-		searchInput.hide();
-		searchButton.removeClass( 'active' );
-	}
 }
 
-$( poncho.init );
+$( Poncho.init );
--- Poncho.php
+++ Poncho.php
@@ -2,31 +2,70 @@
 
 class SkinPoncho extends SkinTemplate {
 
-	public $skinname = 'poncho',
-		$template = 'PonchoTemplate';
+	public $skinname = 'poncho';
 
-	/**
-	 * Initializes OutputPage and sets up skin-specific parameters
-	 *
-	 * @param OutputPage $out
-	 */
-	public function initPage( OutputPage $out ) {
-		$out->addMeta( 'viewport', 'width=device-width' );
+	public $template = 'PonchoTemplate';
 
-		$out->addModuleScripts( 'skins.poncho.scripts' );
-	}
-
-	/**
-	 * @param OutputPage $out
-	 */
-	function setupSkinUserCss( OutputPage $out ) {
-		parent::setupSkinUserCss( $out );
-
-		$out->addModuleStyles( 'skins.poncho.styles' );
+	static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		if ( $skin->getUser()->getOption( 'skin' ) === 'poncho' ) {
+			$out->enableOOUI();
+			$out->addModuleStyles([
+				'skins.poncho.styles',
+				'oojs-ui-core.styles'
+			]);
+			$out->addModules( 'skins.poncho' );
+			$out->addMeta( 'viewport', 'width=device-width' );
+		}
 	}
 }
 
 class PonchoTemplate extends BaseTemplate {
+
+	/**
+	 * Echo the search bar
+	 */
+	function searchInput() {
+		echo new MediaWiki\Widget\SearchInputWidget([
+			'name' => 'search'
+		]);
+	}
+
+	/**
+	 * Echo the User button
+	 */
+	function userButton() {
+		echo new OOUI\ButtonWidget([
+			'id' => 'user-button',
+			'label' => wfMessage( 'poncho-my-account' )->text()
+		]);
+	}
+
+	/**
+	 *
+	 */
+	function skinPath() {
+		global $wgResourceBasePath;
+		echo $wgResourceBasePath . '/skins/Poncho/';
+	}
+
+	/**
+	 * Customize the user menu
+	 */
+	function getUserMenu() {
+		$userMenu = $this->getPersonalTools();
+		unset( $userMenu['uls'] );
+		unset( $userMenu['notifications-alert'] );
+		unset( $userMenu['notifications-notice'] );
+/*
+		unset( $userMenu['userpage'] );
+		unset( $userMenu['mytalk'] );
+		unset( $userMenu['mycontris'] );
+		unset( $userMenu['watchlist'] );
+		unset( $userMenu['anontalk'] );
+		unset( $userMenu['anoncontribs'] );
+*/
+		return $userMenu;
+	}
 
 	/**
 	 * Merge together the views, actions and variants
@@ -37,11 +76,31 @@ class PonchoTemplate extends BaseTemplate {
 		$actions = array_merge(
 			$this->data['content_navigation']['views'],
 			$this->data['content_navigation']['actions'],
-			$this->data['content_navigation']['variants']
+			$this->data['content_navigation']['variants'],
+			$this->data['content_navigation']['namespaces']
 		);
 		$action = $mediaWiki->getAction();
-		unset( $actions[$action] ); // Remove the current action (doesn't work with Move)
+		if ( $action === 'view' ) {
+			unset( $actions['view'] ); // Remove the View action when viewing
+		}
+		unset( $actions['main'] ); // Remove the View action when viewing
 		return $actions;
+	}
+
+	/**
+	 * Get the notifications alert of the Echo extension
+	 */
+	function getNotificationsAlert() {
+		$userLinks = $this->getPersonalTools();
+		return $userLinks['notifications-alert'];
+	}
+
+	/**
+	 * Get the notifications alert of the Echo extension
+	 */
+	function getNotificationsNotice() {
+		$userLinks = $this->getPersonalTools();
+		return $userLinks['notifications-notice'];
 	}
 
 	/**
