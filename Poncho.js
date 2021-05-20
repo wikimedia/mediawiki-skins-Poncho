03b891cff75bfdb2f3775b68aa0be7dd3ea0b954
Jack Phoenix <ashley@uncyclomedia.co>
2021-05-20 17:59:53 +0300

    Indentation style fix: use tabs instead of spaces (+trim EOL whitespace)
    Change-Id: Ia0feb82cc807c031fd0c5dc773ddd40bf45845ed

--- Poncho.js
+++ Poncho.js
@@ -2,61 +2,61 @@
 
 var Poncho = {
 
-    /**
-     * Initialization script
-     */
-    init: function () {
-        Poncho.bind();
-        Poncho.ring();
-        jQuery( '#poncho-search-form input' ).attr( 'list', 'poncho-search-suggestions' );
-    },
+	/**
+	 * Initialization script
+	 */
+	init: function () {
+		Poncho.bind();
+		Poncho.ring();
+		jQuery( '#poncho-search-form input' ).attr( 'list', 'poncho-search-suggestions' );
+	},
 
-    /**
-     * Bind events
-     */
-    bind: function () {
-        jQuery( '#poncho-bell-icon' ).mouseenter( Poncho.readNotifications ); 
-        jQuery( '#poncho-search-form input' ).keyup( Poncho.searchSuggestions ); 
-    },
+	/**
+	 * Bind events
+	 */
+	bind: function () {
+		jQuery( '#poncho-bell-icon' ).mouseenter( Poncho.readNotifications );
+		jQuery( '#poncho-search-form input' ).keyup( Poncho.searchSuggestions );
+	},
 
-    /**
-     * Suggest pages while searching
-     */
-    searchSuggestions: function () {
-        jQuery( '#poncho-search-suggestions' ).empty();
-        var query = jQuery( this ).val();
-    	new mw.Api().get( {
-            action: 'opensearch',
-            search: query
-        } ).done( function ( data ) {
-            var suggestions = data.slice( 1, 2 )[0];
-            suggestions.forEach( function ( suggestion ) {
-                suggestion = jQuery( '<option>' ).val( suggestion );
-                jQuery( '#poncho-search-suggestions' ).append( suggestion );
-            } );
-        } );
-    },
+	/**
+	 * Suggest pages while searching
+	 */
+	searchSuggestions: function () {
+		jQuery( '#poncho-search-suggestions' ).empty();
+		var query = jQuery( this ).val();
+		new mw.Api().get( {
+			action: 'opensearch',
+			search: query
+		} ).done( function ( data ) {
+			var suggestions = data.slice( 1, 2 )[0];
+			suggestions.forEach( function ( suggestion ) {
+				suggestion = jQuery( '<option>' ).val( suggestion );
+				jQuery( '#poncho-search-suggestions' ).append( suggestion );
+			} );
+		} );
+	},
 
-    /**
-     * Mark all notifications of the current user as read and unmark the bell icon
-     */
-    readNotifications: function () {
-    	new mw.Api().postWithEditToken( {
-    		action: 'echomarkread',
-    		all: true,
-    	} );
-        jQuery( '#poncho-bell-icon' ).parent().removeClass( 'active' );
-    },
+	/**
+	 * Mark all notifications of the current user as read and unmark the bell icon
+	 */
+	readNotifications: function () {
+		new mw.Api().postWithEditToken( {
+			action: 'echomarkread',
+			all: true,
+		} );
+		jQuery( '#poncho-bell-icon' ).parent().removeClass( 'active' );
+	},
 
-    /**
-     * Mark the bell icon if the current user has unread notifications
-     */
-    ring: function () {
-        var notificationsItem = jQuery( '#poncho-bell-icon' ).parent();
-        if ( jQuery( 'li.active', notificationsItem ).length ) {
-            notificationsItem.addClass( 'active' );
-        }
-    }
+	/**
+	 * Mark the bell icon if the current user has unread notifications
+	 */
+	ring: function () {
+		var notificationsItem = jQuery( '#poncho-bell-icon' ).parent();
+		if ( jQuery( 'li.active', notificationsItem ).length ) {
+			notificationsItem.addClass( 'active' );
+		}
+	}
 };
 
 mw.loader.using( [
